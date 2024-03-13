517400ae3fd9facc8f8599aef042c150d20bab0d
lukagoderdzishvili <goderdzishvili21@gmail.com>
2024-03-13 17:15:24 +0400

    ADD primeNG & product component

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { ProductsService } from '../services/products.service';
-import { Products } from '../../types';
+import { Product, Products } from '../../types';
+import { ProductComponent } from '../components/product/product.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  
+  products: Product[] = [];
+
   constructor(
     private productsService: ProductsService
   ) {}
@@ -18,6 +23,11 @@ export class HomeComponent {
     this.productsService.getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5}
     ).subscribe((products: Products) => {
       console.log(products.items);
+      this.products = products.items;
     })
+  }
+
+  onProductOutput(product: Product){
+    console.log(product, 'Output')
   }
 }
